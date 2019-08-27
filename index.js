1e0d3402e2ca3d9d2d61b82f33ae0b72cca85ace
sody <bugsabdul@yahoo.com>
2019-08-27 09:24:07 +0100

    disabled cache in express

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ app.use(require('./routes/api'));
 
 
 app.set('view engine', 'ejs');
+app.set('etag', false)
+app.use((req, res, next) => {
+  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private')
+  next()
+})
 app.use('/assets', express.static('assets'));
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
